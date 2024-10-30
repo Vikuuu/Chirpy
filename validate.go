b382cb15f5dbe9230ea79ca3e16595b94a3c2462
Vikuuu <adivik672@gmail.com>
2024-10-31 01:27:23 +0530

    add: The profane

--- validate.go
+++ validate.go
@@ -4,21 +4,15 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"slices"
+	"strings"
 )
 
+type parameters struct {
+	Body string `json:"body"`
+}
+
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	type respError struct {
-		Error string `json:"error"`
-	}
-
-	type respBody struct {
-		Valid bool `json:"valid"`
-	}
-
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
@@ -28,23 +22,41 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if len(params.Body) > 140 {
-		respErr := respError{
-			Error: "chirp is too long",
-		}
-		errData, err := json.Marshal(respErr)
-		if err != nil {
-			log.Printf("Error marshalling JSON: %s", err)
-			w.WriteHeader(500)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(400)
-		w.Write(errData)
+		errResp := "chirp is too long"
+		respondWithError(w, 400, errResp)
 		return
 	}
 
+	respondWithJSON(w, 200, params)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type respError struct {
+		Error string `json:"error"`
+	}
+	respErr := respError{
+		Error: msg,
+	}
+	errData, err := json.Marshal(respErr)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(400)
+	w.Write(errData)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload parameters) {
+	type respBody struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	res := badWordReplacement(payload.Body)
+
 	resBody := respBody{
-		Valid: true,
+		CleanedBody: res,
 	}
 	dat, err := json.Marshal(resBody)
 	if err != nil {
@@ -54,6 +66,21 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
+	w.WriteHeader(code)
 	w.Write(dat)
+}
+
+func badWordReplacement(msg string) string {
+	badWordList := []string{"kerfuffle", "sharbert", "fornax"}
+	// lowerMsg := strings.ToLower(msg)
+	msgList := strings.Split(msg, " ")
+
+	for index, msg := range msgList {
+		if slices.Contains(badWordList, strings.ToLower(msg)) {
+			msgList[index] = "****"
+		}
+	}
+
+	resMsg := strings.Join(msgList, " ")
+	return resMsg
 }
